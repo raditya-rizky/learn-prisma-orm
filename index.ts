ce0829c996a5cdbf61f2d921163b5c49bc2182e0
raditya <gusrizkydharmaputra123@gmail.com>
2022-10-11 19:57:17 +0800

    Prisma from scratch

---
+++ index.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: "Raditya",
+      email: "raditya@email.co",
+      posts: {
+        create: {
+          title: "Hello World",
+          content: "Learn Prisma from scratch!",
+        },
+      },
+      profile: {
+        create: { bio: "I like Black" },
+      },
+    },
+  });
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
+  console.dir(allUsers, { depth: null });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
